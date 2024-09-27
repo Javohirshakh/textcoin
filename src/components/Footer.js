6fe1ed08727064730693dafc754700a27815ad69
Javohir <javohir@MacBook-Air-Javohir.local>
2024-09-27 18:03:56 +0500

    fixed active tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,25 +7,37 @@ function Footer() {
     <nav className="fixed footer bottom-0 left-0 w-full bg-gray-800 p-4">
       <div className="flex justify-around text-center">
         {/* Главная страница */}
-        <NavLink to="/" exact activeClassName="active-tab" className="nav-item">
+        <NavLink 
+          to="/" 
+          className={({ isActive }) => `nav-item ${isActive ? 'active-tab' : ''}`}
+        >
           <span className="material-icons">home</span>
           <p>Textcoin</p>
         </NavLink>
 
         {/* Страница игр */}
-        <NavLink to="/games" activeClassName="active-tab" className="nav-item">
+        <NavLink 
+          to="/games" 
+          className={({ isActive }) => `nav-item ${isActive ? 'active-tab' : ''}`}
+        >
           <span className="material-icons">sports_esports</span>
           <p>O'yinlar</p>
         </NavLink>
 
         {/* Страница бонусов */}
-        <NavLink to="/bonuses" activeClassName="active-tab" className="nav-item">
+        <NavLink 
+          to="/bonuses" 
+          className={({ isActive }) => `nav-item ${isActive ? 'active-tab' : ''}`}
+        >
           <span className="material-icons">card_giftcard</span>
           <p>Bonuslar</p>
         </NavLink>
 
         {/* Страница пользователя */}
-        <NavLink to="/userPage" activeClassName="active-tab" className="nav-item">
+        <NavLink 
+          to="/userPage" 
+          className={({ isActive }) => `nav-item ${isActive ? 'active-tab' : ''}`}
+        >
           <span className="material-icons">account_circle</span>
           <p>User Page</p>
         </NavLink>
