a8699bd71ed5feaad02dca2417447e2aeadff8cd
Javohir <javohir@MacBook-Air-Javohir.local>
2024-10-02 11:19:58 +0500

    telegram context test

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function HomePage() {
+  const [userName, setUserName] = useState('');
+
+  useEffect(() => {
+    // Проверяем, доступен ли объект Telegram для получения данных о пользователе
+    if (window.Telegram && window.Telegram.WebApp && window.Telegram.WebApp.initDataUnsafe) {
+      const user = window.Telegram.WebApp.initDataUnsafe.user;
+      setUserName(user ? user.first_name : 'Гость'); // Получаем имя пользователя, или отображаем 'Гость'
+    }
+  }, []);
+
   return (
     <>
+      <h1 className="text-2xl font-bold">Привет, {userName}!</h1>
       <p className="text-gray-400">Balans</p>
       <h2 className="text-3xl font-bold">14 800 uzs</h2>
       <button className="custom-button text-white py-2 px-6 rounded-full mt-6">Yutuqlaringiz</button>
