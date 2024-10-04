4bdecfd5d4e27a0327377310fea21ca6c3e634df
Javohir <javohir@MacBook-Air-Javohir.local>
2024-10-05 01:26:22 +0500

    added Tasks page

--- src/pages/TasksPage.js
+++ src/pages/TasksPage.js
@@ -1,14 +1,63 @@
 import React from 'react';
+import './tasks.css';
 
 function TasksPage() {
+  // Массив с задачами
+  const tasks = [
+    { 
+      name: "TextCoin telegram kanaliga obuna bo'ling!", 
+      reward: "+5000 UZS", 
+      actionLink: "https://t.me/bmwgacha",
+      check: false 
+    },
+    { 
+      name: "TextCoin instagramiga obuna bo'ling!", 
+      reward: "+300 UZS", 
+      actionLink: "https://instagram.com/",
+      check: false 
+    },
+    { 
+      name: "TextCoin youtubega obuna bo'ling!", 
+      reward: "+700 UZS", 
+      actionLink: "https://youtube.com/",
+      check: false 
+    }
+  ];
+
+  // Обработчик клика по кнопке "Check"
+  const handleCheck = (index) => {
+    // Обновление задачи при нажатии "Check"
+    tasks[index].check = true;
+    alert(`Siz ${tasks[index].name} uchun ${tasks[index].reward} oldingiz!`);
+  };
+
   return (
-    <>
-      <h2 className="text-3xl font-bold">Vazifalar</h2>
-      
-    </>
+    <div className="tasks-container">
+      <h2>Vazifalar</h2>
+      {tasks.map((task, index) => (
+        <div key={index} className="task-item">
+          <div className="task-info">
+            <p className="task-name">{task.name}</p>
+            <div className='task-bottom'>
+            <span className="task-reward">{task.reward}</span>
+            <button 
+              className="check-button"
+              onClick={() => handleCheck(index)}
+            >
+              Tekshirish
+            </button>
+            </div>
+          </div>
+          <button 
+            className="task-button" 
+            onClick={() => window.open(task.actionLink, "_blank")}
+          >
+            Join
+          </button>
+        </div>
+      ))}
+    </div>
   );
 }
 
 export default TasksPage;
-
-
