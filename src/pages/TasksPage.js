1855e755f2d2ce2a0ca56d2698bae4e547060f3a
Javohir <javohir@MacBook-Air-Javohir.local>
2024-10-21 02:43:41 +0500

    fixed tasks page

--- src/pages/TasksPage.js
+++ src/pages/TasksPage.js
@@ -26,7 +26,7 @@ function TasksPage() {
           const sortedTasks = result.gettask.sort((a, b) => a.status === b.status ? 0 : a.status ? -1 : 1);
           setTasks(sortedTasks);
         } else {
-          alert("Vazifalar topilmadi.");
+          console.error("Vazifalar topilmadi.");
         }
       } catch (error) {
         console.error("Ошибка API:", error);
@@ -54,7 +54,7 @@ function TasksPage() {
       }
     } catch (error) {
       console.error("Ошибка при проверке задачи:", error);
-      alert("Xatolik yuz berdi. Keyinroq urinib ko'ring.");
+      alert("Xatolik yuz berdi. Keyinroq urinib ko'rинг.");
     }
   };
 
@@ -66,35 +66,39 @@ function TasksPage() {
     <>
       <h2 className="text-3xl font-bold">Vazifalar</h2> 
       <div className="tasks-container mt-4 mb-8 pb-2 rounded-lg">
-        {tasks.map((task, index) => (
-          <div key={index} className="task-item">
-            <div className="task-info">
-              <span className="task-name">{task.task}</span>
-              <div className="task-bottom">
-                <span className="task-reward">{task.summa} UZS</span>
-                {task.status ? (
-                  <button 
-                    className="check-button"
-                    onClick={() => handleCheck(task.task_id, index)} // Передаем task_id для проверки
-                  >
-                    Tekshirish
-                  </button>
-                ) : (
-                  <span className="task-done text-green-500">Bajarildi</span>
-                )}
+        {tasks.length === 0 ? ( // Проверка наличия задач
+          <p className="text-gray-400">Vazifalar topilmadi!</p>
+        ) : (
+          tasks.map((task, index) => (
+            <div key={index} className="task-item">
+              <div className="task-info">
+                <span className="task-name">{task.task}</span>
+                <div className="task-bottom">
+                  <span className="task-reward">{task.summa} UZS</span>
+                  {task.status ? (
+                    <button 
+                      className="check-button"
+                      onClick={() => handleCheck(task.task_id, index)} // Передаем task_id для проверки
+                    >
+                      Tekshirish
+                    </button>
+                  ) : (
+                    <span className="task-done text-green-500">Bajarildi</span>
+                  )}
+                </div>
               </div>
+              {task.status && (
+                <button 
+                  className="task-button bg-green-500 hover:bg-green-700 text-white py-1 px-3 rounded ml-4" 
+                  onClick={() => window.open(task.url, "_blank")} target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Bajarish
+                </button>
+              )}
             </div>
-            {task.status && (
-              <button 
-                className="task-button bg-green-500 hover:bg-green-700 text-white py-1 px-3 rounded ml-4" 
-                onClick={() => window.open(task.url, "_blank")} target="_blank"
-                rel="noopener noreferrer"
-              >
-                Bajarish
-              </button>
-            )}
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </>
   );
